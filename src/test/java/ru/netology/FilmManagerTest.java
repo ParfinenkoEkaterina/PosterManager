74f366fdc45464f8149df94d277fb4d2808f27c1
Ekaterina Parfinenko <katyavasilenko2012@gmail.com>
2023-02-22 10:08:17 +0300

    new tests

--- src/test/java/ru/netology/FilmManagerTest.java
+++ src/test/java/ru/netology/FilmManagerTest.java
@@ -3,6 +3,8 @@ package ru.netology;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 public class FilmManagerTest {
 
     @Test
@@ -20,7 +22,7 @@ public class FilmManagerTest {
         manager.addFilm(film4);
         manager.addFilm(film5);
 
-        String[] expected = { "Film I", "Film II", "Film III",  "Film IV", "Film V" };
+        String[] expected = {"Film I", "Film II", "Film III", "Film IV", "Film V"};
         String[] actual = manager.findAll();
 
         Assertions.assertArrayEquals(expected, actual);
@@ -40,10 +42,141 @@ public class FilmManagerTest {
 
         manager.addFilm(film5);
 
-        String[] expected = { "Film V" };
+        String[] expected = {"Film V"};
         String[] actual = manager.findLast();
 
         Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void shouldGiveLimit() {
+
+        String film1 = "Film I";
+        String film2 = "Film II";
+        String film3 = "Film III";
+        String film4 = "Film IV";
+        String film5 = "Film V";
+        String film6 = "Film VI";
+        String film7 = "Film VII";
+        String film8 = "Film VIII";
+        String film9 = "Film IX";
+        String film10 = "Film X";
+        String film11 = "Film XI";
+
+        FilmManager manager = new FilmManager();
+        manager.addFilm(film1);
+        manager.addFilm(film2);
+        manager.addFilm(film3);
+        manager.addFilm(film4);
+        manager.addFilm(film5);
+        manager.addFilm(film6);
+        manager.addFilm(film7);
+        manager.addFilm(film8);
+        manager.addFilm(film9);
+        manager.addFilm(film10);
+        manager.addFilm(film11);
+
+
+        int expected = 10;
+        int actual = manager.findLast().length;
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+
+    @Test
+    public void shouldGiveDifferentLimit() {
+
+        String film1 = "Film I";
+        String film2 = "Film II";
+        String film3 = "Film III";
+        String film4 = "Film IV";
+        String film5 = "Film V";
+        String film6 = "Film VI";
+        String film7 = "Film VII";
+        String film8 = "Film VIII";
+        String film9 = "Film IX";
+        String film10 = "Film X";
+        String film11 = "Film XI";
+
+        FilmManager manager = new FilmManager();
+        manager.addFilm(film1);
+        manager.addFilm(film2);
+        manager.addFilm(film3);
+        manager.addFilm(film4);
+        manager.addFilm(film5);
+        manager.addFilm(film6);
+        manager.addFilm(film7);
+        manager.addFilm(film8);
+        manager.addFilm(film9);
+        manager.addFilm(film10);
+        manager.addFilm(film11);
+
+
+
+        int expected = 11;
+        int actual = manager.findAll().length;
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void shouldAllLimit() {
+
+        String film1 = "Film I";
+        String film2 = "Film II";
+        String film3 = "Film III";
+        String film4 = "Film IV";
+        String film5 = "Film V";
+        String film6 = "Film VI";
+        String film7 = "Film VII";
+        String film8 = "Film VIII";
+        String film9 = "Film IX";
+        String film10 = "Film X";
+        String film11 = "Film XI";
+
+        FilmManager manager = new FilmManager();
+        manager.addFilm(film1);
+        manager.addFilm(film2);
+        manager.addFilm(film3);
+        manager.addFilm(film4);
+        manager.addFilm(film5);
+        manager.addFilm(film6);
+        manager.addFilm(film7);
+        manager.addFilm(film8);
+        manager.addFilm(film9);
+        manager.addFilm(film10);
+        manager.addFilm(film11);
+
+
+
+
+       String[] expected = {"Film I", "Film II", "Film III", "Film IV", "Film V", "Film VI", "Film VII", "Film VIII", "Film IX", "Film X", "Film XI"};
+       String[] actual = manager.findAll();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldGiveThisLimit() {
+        String film1 = "Film I";
+        String film2 = "Film II";
+        String film3 = "Film III";
+
+        FilmManager manager = new FilmManager();
+        manager.addFilm(film1);
+        manager.addFilm(film2);
+        manager.addFilm(film3);
+
+        int limit = 3;
+
+        int expected = 3;
+        Arrays.stream(new FilmManager[]{new FilmManager()}).limit(3);
+
+
 
     }
 
